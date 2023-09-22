38c74c514853e4c86f1681f19be198bb6948c9a1
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2023-09-22 13:49:47 +0200

    Finished "REST API konsumieren Vanilla JS".

--- 03-REST-API-Vanilla-JS-Basics/script.js
+++ 03-REST-API-Vanilla-JS-Basics/script.js
@@ -1,7 +1,16 @@
 document.getElementById("load").addEventListener("click", function () {
-  fetch("https://jsonplaceholder.typicode.com/todos/").then((result) => {
+  fetch("https://jsonplaceholder.typicode.com/todos").then((result) => {
     result.json().then((data) => {
-      console.log(data);
+      getHTMLforTodos(data);
     });
   });
 });
+
+function getHTMLforTodos(data) {
+  let html = "<ul>";
+  data.forEach((todo) => {
+    html += "<li>" + todo.title + "</li>";
+  });
+  html += "</ul>";
+  document.getElementById("content").innerHTML = html;
+}
