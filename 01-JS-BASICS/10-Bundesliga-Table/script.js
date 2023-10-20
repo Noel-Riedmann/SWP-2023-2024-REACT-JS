1700a994b849a3607e1df7b427fae9dd92a0a56c
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2023-10-20 14:46:03 +0200

    Finished table w.i.p. on goalgetters.

--- 01-JS-BASICS/10-Bundesliga-Table/script.js
+++ 01-JS-BASICS/10-Bundesliga-Table/script.js
@@ -1,3 +1,65 @@
-function navigateTo(location){
-    window.location.href = location;
-};
+function getLastHtmlPart() {
+    var parentPathname = window.parent ? window.parent.location.pathname : window.location.pathname;
+    return parentPathname.split("/").pop();
+  }
+  
+  function navigateTo(location) {
+    var lastHtmlPart = getLastHtmlPart();
+  
+    if (location !== lastHtmlPart) {
+      if (window.parent) {
+        window.parent.location.href = location; 
+      } else {
+        window.location.href = location; 
+      }
+    }
+  
+    document.querySelectorAll(".menuButton").forEach((button) => {
+      button.classList.remove("active");
+    });
+  
+    document
+      .getElementById(lastHtmlPart.replace(".html", "Button"))
+      .classList.add("active");
+  }
+  
+  window.onload = function () {
+    navigateTo(getLastHtmlPart());
+  };
+  
+  const loadRows = () => {
+    fetch("https://api.openligadb.de/getbltable/%C3%B6bl1/2023")
+      .then((result) => result.json())
+      .then((data) => {
+        fillTable(data);
+      })
+      .catch((error) => {
+        console.error('Error loading data:', error);
+      });
+  };
+  
+  const fillTable = (data) => {
+    let html = "";
+  
+    data.forEach((element, index) => {
+      html += `    
+        <div class="tR">
+        <div class="pos">${index + 1}</div>
+        <div class="name"><div><img src="${
+          element.teamIconUrl
+        }" alt="logo" class="teamLogo"></div>&nbsp;&nbsp;
+          ${element.teamName}</div>
+        <div class="MP">${element.matches}</div>
+        <div class="W">${element.won}</div>
+        <div class="D">${element.draw}</div>
+        <div class="L">${element.lost}</div>
+        <div class="GF">${element.goals}</div>
+        <div class="GA">${element.opponentGoals}</div>
+        <div class="diff">${element.goals}:${element.opponentGoals}</div>
+        <div class="PTS">${element.points}</div>
+        </div>`;
+    });
+    document.getElementById("rows").innerHTML = html;
+  };
+  
+loadRows();
