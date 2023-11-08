c025342da07d1aad14e33a873787d6de3c1ed69b
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2023-11-08 11:58:10 +0100

    Added goalgetters.

--- 01-JS-BASICS/10-Bundesliga-Table/script.js
+++ 01-JS-BASICS/10-Bundesliga-Table/script.js
@@ -1,48 +1,50 @@
 function getLastHtmlPart() {
-    var parentPathname = window.parent ? window.parent.location.pathname : window.location.pathname;
-    return parentPathname.split("/").pop();
-  }
-  
-  function navigateTo(location) {
-    var lastHtmlPart = getLastHtmlPart();
-  
-    if (location !== lastHtmlPart) {
-      if (window.parent) {
-        window.parent.location.href = location; 
-      } else {
-        window.location.href = location; 
-      }
+  var parentPathname = window.parent
+    ? window.parent.location.pathname
+    : window.location.pathname;
+  return parentPathname.split("/").pop();
+}
+
+function navigateTo(location) {
+  var lastHtmlPart = getLastHtmlPart();
+
+  if (location !== lastHtmlPart) {
+    if (window.parent) {
+      window.parent.location.href = location;
+    } else {
+      window.location.href = location;
     }
-  
-    document.querySelectorAll(".menuButton").forEach((button) => {
-      button.classList.remove("active");
-    });
-  
-    document
-      .getElementById(lastHtmlPart.replace(".html", "Button"))
-      .classList.add("active");
   }
-  
-  window.onload = function () {
-    navigateTo(getLastHtmlPart());
-  };
-  
-  const loadRows = () => {
-    fetch("https://api.openligadb.de/getbltable/%C3%B6bl1/2023")
-      .then((result) => result.json())
-      .then((data) => {
-        fillTable(data);
-      })
-      .catch((error) => {
-        console.error('Error loading data:', error);
-      });
-  };
-  
-  const fillTable = (data) => {
-    let html = "";
-  
-    data.forEach((element, index) => {
-      html += `    
+
+  document.querySelectorAll(".menuButton").forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  document
+    .getElementById(lastHtmlPart.replace(".html", "Button"))
+    .classList.add("active");
+}
+
+window.onload = function () {
+  navigateTo(getLastHtmlPart());
+};
+
+const loadRows = () => {
+  fetch("https://api.openligadb.de/getbltable/%C3%B6bl1/2023")
+    .then((result) => result.json())
+    .then((data) => {
+      fillTable(data);
+    })
+    .catch((error) => {
+      console.error("Error loading data:", error);
+    });
+};
+
+const fillTable = (data) => {
+  let html = "";
+
+  data.forEach((element, index) => {
+    html += `    
         <div class="tR">
         <div class="pos">${index + 1}</div>
         <div class="name"><div><img src="${
@@ -58,8 +60,34 @@ function getLastHtmlPart() {
         <div class="diff">${element.goals}:${element.opponentGoals}</div>
         <div class="PTS">${element.points}</div>
         </div>`;
-    });
-    document.getElementById("rows").innerHTML = html;
-  };
-  
+  });
+  document.getElementById("rows").innerHTML = html;
+};
+
 loadRows();
+
+const loadGoalgetters = () => {
+  fetch("https://api.openligadb.de/getgoalgetters/%C3%B6bl1/2023")
+    .then((result) => result.json())
+    .then((data) => {
+      fillGoalgetters(data);
+    })
+    .catch((error) => {
+      console.error("Error loading data:", error);
+    });
+};
+
+const fillGoalgetters = (data) => {
+  let html = "";
+
+  data.forEach((element, index) => {
+    html += `
+    <div class="goalTR">
+    <div class="playerPosition">${index + 1}</div>
+    <div class="playerName">${element.goalGetterName}</div>
+    <div class="playerGoals">${element.goalCount}</div></div>`;
+  });
+  document.getElementById("goalTR").innerHTML = html;
+};
+
+loadGoalgetters();
