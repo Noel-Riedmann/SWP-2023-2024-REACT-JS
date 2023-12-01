2f897e079b2bfb59270fcae1e7e195b377d5b547
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2023-12-01 14:45:52 +0100

    Added list.

--- 02-REACT/02-design-practice/src/App.js
+++ 02-REACT/02-design-practice/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import TestList from './components/TestList/TestList';
 
+
 function App() {
   return (
     <div className="App">
--- 02-REACT/02-design-practice/src/components/TestList/TestList.js
+++ 02-REACT/02-design-practice/src/components/TestList/TestList.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import styles from './TestList.module.css'
+import React from 'react';
+import styles from './TestList.module.css';
 
 export default function TestList() {
   return (
-    <div>
-      ul
+    <div className={styles.testList}>
+      <ul>
+        <li>Cheese</li>
+        <li>Milk</li>
+        <li>Pork</li>
+      </ul>
     </div>
-  )
+  );
 }
