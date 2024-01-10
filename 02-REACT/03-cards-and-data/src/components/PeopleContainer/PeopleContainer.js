a42f8a169ac903418df65025ea28c9fb2fd2451c
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2024-01-10 11:54:14 +0100

    Cards and Data: added filter.

--- 02-REACT/03-cards-and-data/src/components/PeopleContainer/PeopleContainer.js
+++ 02-REACT/03-cards-and-data/src/components/PeopleContainer/PeopleContainer.js
@@ -4,27 +4,48 @@ import Card from "../Card/Card";
 
 export default function PeopleContainer(props) {
   const [people, setPeople] = useState([]);
+  const [filteredPeople, setFilteredPeople] = useState([]);
   useEffect(() => {
     fetch("https://659e6ae847ae28b0bd35c8fa.mockapi.io/person").then((result) =>
       result.json().then((data) => {
         setPeople(data);
+        setFilteredPeople(data);
       })
     );
   }, {});
 
+  const filterPeople = (filter) => {
+    let filtered = people.filter((person) => person.name.includes(filter));
+    setFilteredPeople(filtered);
+  };
+
   return (
     <div>
-      <h1>People</h1>
-      <div className={style.container}>
-        {people.map((element) => {
-          return (
-            <Card
-              firstname={element.name}
-              pos={element.position}
-              img={element.avatar}
-            ></Card>
-          );
-        })}
+      <div className={style.header}>
+        <h1>People</h1>
+        <div>
+          <input
+            className={style.searchField}
+            type="text"
+            placeholder="Search"
+            onChange={(element) => {
+              filterPeople(element.target.value);
+            }}
+          ></input>
+        </div>
+      </div>
+      <div className={style.list}>
+        <div className={style.container}>
+          {filteredPeople.map((element) => {
+            return (
+              <Card
+                firstname={element.name}
+                pos={element.position}
+                img={element.avatar}
+              ></Card>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
