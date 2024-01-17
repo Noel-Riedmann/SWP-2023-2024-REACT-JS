b8def5c0cd1ae9bdd15304d51995209cb44b64f1
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2024-01-17 10:49:06 +0100

    Added missing line on Design Practice.

--- 02-REACT/02-design-practice/src/App.js
+++ 02-REACT/02-design-practice/src/App.js
@@ -1,16 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import TestList from './components/TestList/TestList';
-import TestButton from  './components/TestButton/TestButton';
-import PersonCard from './components/PersonCard/PersonCard'
-
+import logo from "./logo.svg";
+import "./App.css";
+import TestList from "./components/TestList/TestList";
+import TestButton from "./components/TestButton/TestButton";
+import PersonCard from "./components/PersonCard/PersonCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <TestList></TestList>
-      <TestButton></TestButton>
+        <PersonCard
+          name="Noel Riedmann"
+          title="HTL Student & Webdeveloper"
+          img="https://www.w3schools.com/howto/img_avatar.png"
+        ></PersonCard>{" "}
+        <TestList></TestList>
+        <TestButton></TestButton>
       </header>
     </div>
   );
